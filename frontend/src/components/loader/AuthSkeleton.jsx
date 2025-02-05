31e332a513f7718e3a321aaf53ad412178bf1e47
Monika-Dangar <dangarmonika191023@gmail.com>
2025-02-05 20:16:56 +0530

    Added loader while verfiying credentials

---
+++ frontend/src/components/loader/AuthSkeleton.jsx
@@ -0,0 +1,13 @@
+
+const AuthSkeleton = () => {
+    return (
+        <>
+            <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-50 z-10">
+                <div className="w-16 h-16 border-4 border-t-4 border-gray-300 border-t-[#6595ac] rounded-full animate-spin"></div>
+            </div>
+
+        </>
+    )
+}
+
+export default AuthSkeleton
